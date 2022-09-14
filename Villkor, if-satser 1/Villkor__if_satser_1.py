581fb06a0b1f1ab824e0b626577ea606f3895dfe
Admin <Admin@TEB123012>
2022-09-14 10:43:25 +0200

    Add project files.

---
+++ Villkor, if-satser 1/Villkor__if_satser_1.py
@@ -0,0 +1,13 @@
+
+a= int(input("a = "))
+b= int(input("b = "))
+c= int(input("c = "))
+
+
+if a==c and b==c: 
+    print("a och b = c") 
+elif a==b or b==c:
+    print("a eller b = c")
+else:
+    print("Nej")
+  
